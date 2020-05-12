604945084826d9d8f54f61bedafa694b9c168308
The Oracle Software Owner User <oracle@mw-instance.sub05080509280.mwvcn.oraclevcn.com>
2020-05-12 02:33:22 +0000

    Add database connection

--- promotion.py
+++ promotion.py
@@ -2,7 +2,7 @@
 Simple Python application to show CI/CD capabilities.
 """
 
-
+import cx_Oracle
 from bottle import Bottle, run
 
 app = Bottle()
@@ -23,5 +23,17 @@ def decrease(salary, amount):
     return str(int(salary) - int(amount))
 
 
+@app.route('/conn')
+def conn():
+    return str(connection.version)
+
+
 if __name__ == '__main__':
+    DBUSER = 'hr'
+    DBPASS = 'WWelcome##2018' 
+    DBHOST = '168.138.38.174:1521'
+    DBSERV = 'pdb01.sub05080509280.mwvcn.oraclevcn.com'
+    conn_string = DBUSER + '/' + DBPASS + '@//' + DBHOST + '/' + DBSERV
+    connection = cx_Oracle.connect(conn_string)
     run(app, host='0.0.0.0', port=8080)
+    connection.close()
