4061335428c2704edde4228a3b673897fde9801f
The Oracle Software Owner User <oracle@doc-compute.sub02250147130.docvcn.oraclevcn.com>
2021-02-25 06:42:58 +0000

    test results

--- promotion.py
+++ promotion.py
@@ -2,8 +2,11 @@
 Simple Python application to show CI/CD capabilities.
 Second time around
 """
-ddition(salary, amount):
+
+
+def addition(salary, amount):
     return salary + amount
+
 
 def increment(salary, percentage):
     return salary * (1 + percentage/100)
