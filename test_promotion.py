9450c22fcc5c7e3b48e727fdddcd7afb0ccc8f8a
The Oracle Software Owner User <oracle@doc-compute.sub02250147130.docvcn.oraclevcn.com>
2021-02-25 17:52:54 +0000

    add test

--- test_promotion.py
+++ test_promotion.py
@@ -7,9 +7,8 @@ import promotion
 
 class TestPromotion:
 
-   def test_addition(self):
-      assert 1200 == promotion.addition(1150, 50)
+    def test_addition(self):
+        assert 1200 == promotion.addition(1150, 50)
 
-
-   def test_increment(self):
-      assert 1250 == promotion.increment(1000, 25)
+    def test_increment(self):
+        assert 1250 == promotion.increment(1000, 25)
