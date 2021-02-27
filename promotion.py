46c02401a285886587cac1557fa412f8ee647649
The Oracle Software Owner User <oracle@doc-compute.sub02250147130.docvcn.oraclevcn.com>
2021-02-27 03:08:19 +0000

    Add 21c client

--- promotion.py
+++ promotion.py
@@ -2,9 +2,15 @@
 Simple Python application to show CI/CD capabilities.
 """
 
+import cx_Oracle
 from bottle import Bottle, run
 
 app = Bottle()
+
+
+@app.route('/conn')
+def conn():
+    return str(connection.version)
 
 
 @app.route('/addition/<salary>/<amount>')
@@ -23,4 +29,11 @@ def decrease(salary, amount):
 
 
 if __name__ == '__main__':
+    DBUSER = 'hr'
+    DBPASS = 'WWelcome##2018'
+    DBHOST = 'DBCS_sjc1nw.sub02250147130.docvcn.oraclevcn.com'
+    DBSERV = 'pdb01.sub02250147130.docvcn.oraclevcn.com'
+    conn_string = DBUSER + '/' + DBPASS + '@//' + DBHOST + '/' + DBSERV
+    connection = cx_Oracle.connect(conn_string)
     run(app, host='0.0.0.0', port=8080)
+    connection.close()
